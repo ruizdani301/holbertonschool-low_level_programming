4113d6f0c757bc54ef77f76dbf7d97a015b33249
ruizdani301 <ruizdani301@gmail.com>
2021-10-01 15:05:30 -0500

    print

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+*main - program that prints number 0 - 9,
+*Return: 0
+*/
+
+int main(void)
+{
+	
+	for (a = 0; a<=9; a++)
+	{
+		printf(a);
+
+	}
+	putchar("\n")
+	return (0)
+}
+	
