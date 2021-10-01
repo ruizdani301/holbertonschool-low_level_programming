beb36a787dc82ed9257ddcd7578921a5535a8a66
ruizdani301 <ruizdani301@gmail.com>
2021-10-01 16:59:48 -0500

    print

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+*main - program that prints the alphabet in lowercase,
+*Return: 0
+*/
+
+int main(void)
+
+{
+	int a;
+
+	for (a = 97 ; a <= 122 ; a++)
+	{
+		putchar(a);
+
+	}
+	putchar(10);
+	return (0);
+}
