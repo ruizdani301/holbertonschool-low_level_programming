89ed4974caa40e8c6f0394c1dd6b4faf85120cd4
ruizdani301 <ruizdani301@gmail.com>
2021-10-04 13:59:57 -0500

    ./0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+*main - program that prints _putchar,
+*Return: 0
+*/
+
+int main(void)
+
+{
+	printf("_putchar\n");
+	return (0);
+}
