73767e03f2bd6bc1afadb7b92e5cabf3616eb741
ruizdani301 <ruizdani301@gmail.com>
2021-10-05 09:29:49 -0500

    islower

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,16 +1,13 @@
 #include "main.h"
-
 /**
 *_islower -  compare, in lowercase,
 *@c: variable
 *Return: 0
 */
 
-void int _islower(int c)
+int _islower(int c)
 {
-	int c;
-
-	if (c >= a && c <= z)
+	if (c >= 'a' && c <= 'z')
 	{
 		return (1);
 	}
@@ -18,3 +15,4 @@ void int _islower(int c)
 	{
 		return (0);
 	}
+}
