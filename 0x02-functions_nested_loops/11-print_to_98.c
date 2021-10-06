21467328c0f2c7ed57f8f7a0aed6f7ee18e85201
ruizdani301 <ruizdani301@gmail.com>
2021-10-05 19:40:32 -0500

    98

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,30 @@
+#include "main.h"
+/**
+ *print_to_98 - function that return (i + z)
+ *@n: data one
+ *Return: 98
+ */
+void print_to_98(int n)
+
+{
+	if (n < 98)
+	{
+		for (n = n; n < 98; n++)
+		{
+			_putchar('n');
+			_putchar(',');
+			_putchar(' ');
+		}
+	}
+	_putchar(10);
+	if (n > 98)
+	{
+		for (n = n; n > 98; n--)
+		{
+		_putchar('n');
+		_putchar(',');
+		_putchar(' ');
+	}
+	_putchar(10);
+}
+}
