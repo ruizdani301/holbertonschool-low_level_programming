c407040c36bab54b390146e5a064befcec4cab4a
ruizdani301 <ruizdani301@gmail.com>
2021-10-06 18:58:49 -0500

    times

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -15,18 +15,15 @@ int c;
 			if (c <= 9)
 			{
 				_putchar((c % 10) + '0');
-				_putchar(' ');
 			}
-				if (c > 9)
+			if (c > 9)
 				{
 				_putchar((c / 10) + '0');
 				_putchar((c % 10) + '0');
-				_putchar(',');
-				_putchar(' ');
 				}
-			if(i < 9)
+			if (i < 9)
 			{
-				 _putchar(',')
+				_putchar(',');
 			}
 			}
 		_putchar('\n');
