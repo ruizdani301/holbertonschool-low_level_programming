323ed3c1f638ff4d2fadddc982f9917ef836dcd2
ruizdani301 <ruizdani301@gmail.com>
2021-10-06 12:22:07 -0500

    print

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * times_table - prints tables
+ */
+void times_table(void)
+{
+	int a = 0;
+	int i = 0;
+int c = 0;
+	for (a = 0; a <= 9; a++)
+	{
+		for (i = 0; i <= 9; i++)
+		{
+			c = (a * i);
+			if (c <= 9)
+			{
+				_putchar((c % 10) + '0');
+				_putchar(',');
+				_putchar(' ');
+			}
+			_putchar((c / 10) + '0');
+			_putchar((c % 10) + '0');
+			_putchar(',');
+		}	_putchar(10);
+	}
+}
