551261fb565cddb8e1c6398d750dc086acee9635
ruizdani301 <ruizdani301@gmail.com>
2021-10-09 13:13:39 -0500

    number

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ *print_most_numbers - prints the numbers, from 0 to 9, least 2 and 4 followed by a new line
+ */
+void print_most_numbers(void)
+{
+	int a;
+
+	for (a = 48; a <= 57; a++)
+	{
+		if (a == 50 || a == 52)
+		{
+			a++;
+		}
+	}
+	_putchar(a);
+	_putchar(10);
+}
