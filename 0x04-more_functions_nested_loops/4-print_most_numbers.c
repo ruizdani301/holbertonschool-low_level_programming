994b54e6b0bd90235e73f564cfeb0a73436a72de
ruizdani301 <ruizdani301@gmail.com>
2021-10-09 13:36:13 -0500

    print

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- *print_most_numbers - prints the numbers, from 0 to 9, least 2 and 4 followed by a new line
+ *print_most_numbers - prints 0 to 9, least 2 and 4 followed by a new line
  */
 void print_most_numbers(void)
 {
