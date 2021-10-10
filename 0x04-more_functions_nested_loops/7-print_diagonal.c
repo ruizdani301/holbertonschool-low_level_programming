782907127fe01860d0be6feb7cf8e7897996bb3b
ruizdani301 <ruizdani301@gmail.com>
2021-10-10 14:41:01 -0500

    diagonal

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,31 @@
+#include "main.h"
+/**
+ * print_diagonal - print diagonal line on the terminal
+ * @n: data
+ */
+void print_diagonal(int n)
+{
+	int i = 0;
+	int x = 0;
+
+	for (i = 0; i < n; i++)
+
+	{
+		for (x = 0; x <= i; x++)
+		{
+			if (x < i)
+			{
+				_putchar(32);
+			}
+			else
+			{
+				_putchar(92);
+			}
+			putchar(10);
+		}
+	}
+	if (n <= 0)
+	{
+		_putchar(10);
+	}
+}
