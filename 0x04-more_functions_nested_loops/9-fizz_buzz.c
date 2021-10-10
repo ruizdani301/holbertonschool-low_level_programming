50f087528b2add1198d06c2c0238837a63a83dc3
ruizdani301 <ruizdani301@gmail.com>
2021-10-09 19:02:05 -0500

    fizz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -9,27 +9,29 @@ int main(void)
 
 	for (n = 1; n <= 100; n++)
 	{
+		
 		if (n % 3 == 0 && n % 5 == 0)
 		{
 			printf("FizzBuzz");
-			printf(" ");
 		}
 		else if (n % 3 == 0)
 		{
 			printf("Fizz");
-			printf(" ");
 		}
 		else if (n % 5 == 0)
 		{
 			printf("Buzz");
-			printf(" ");
 		}
 		else
 		{
 			printf("%d", n);
+		}
+		if (n < 100)
+		{
 			printf(" ");
 		}
 	}
+	
 	printf("\n");
 	return (0);
 }
