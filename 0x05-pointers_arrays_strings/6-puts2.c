42012254852e8a4a2517932910f159ddf4ca0800
ruizdani301 <ruizdani301@gmail.com>
2021-10-12 17:29:09 -0500

    puts

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -10,14 +10,12 @@ void puts2(char *str)
 	int i = 0;
 	int c;
 
-	if (str != ' ')
+	while (str[i + 1] != '\0')
+		i++;
+	for (c = 0; c <= i; c += 2)
 	{
-		while (str[i + 1] != '\0')
-			i++;
-		for (c = 0; c <= i; c += 2)
-		{
-			_putchar(str[c]);
-		}
+		_putchar(str[c]);
 	}
+
 	_putchar(10);
 }
