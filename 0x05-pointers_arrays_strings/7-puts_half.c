faac0903cecab0097ffd278ba549a8b543c1f0e2
ruizdani301 <ruizdani301@gmail.com>
2021-10-12 19:59:29 -0500

    mitad

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -16,7 +16,7 @@ void puts_half(char *str)
 	if (i % 2 == 0)
 	{
 		a = (i / 2);
-		for (c = a; c <= i; c++)
+		for (c = a; c < i; c++)
 		{
 			_putchar(str[c]);
 		}
@@ -26,7 +26,7 @@ void puts_half(char *str)
 	{
 		a = ((i - 1) / 2);
 
-		for (c = a; c <= i; c++)
+		for (c = a; c < i; c++)
 		{
 			_putchar(str[c]);
 		}
