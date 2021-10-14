542796d7b0991e412f74470c713dff95b48dc44f
ruizdani301 <ruizdani301@gmail.com>
2021-10-14 16:01:37 -0500

    strncpy

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -8,11 +8,11 @@
 * Return: 0
 */
 char *_strncpy(char *dest, char *src, int n)
-{	
+{
 	char *ptr1 = dest;
 	char *ptr2 = src;
 	int i = 0;
-		
+
 	while (i < n && *ptr2 != '\0')
 	{
 		*ptr1 = *ptr2;
@@ -23,8 +23,8 @@ char *_strncpy(char *dest, char *src, int n)
 	while (i < n)
 	{
 		*ptr1 = '\0';
-		ptr++;
-		i++
+		ptr1++;
+		i++;
 	}
 	return (dest);
 }
