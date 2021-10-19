0bb45a9b8043a8c139f3331eebebe77683fe8d93
ruizdani301 <ruizdani301@gmail.com>
2021-10-19 00:08:03 -0500

    strspn

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdio.h>
+/**
+* _strspn- toupper.
+* @accept: variable
+* @s: variable
+* Return: (data)
+*/
+unsigned int _strspn(char *s, char *accept)
+{
+	int i, x, n = 0;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (x = 0; accept[x] != '\0'; x++)
+		{
+			if (s[i] == accept[x])
+			{
+				n++;
+			}
+		}
+	}
+
+	return (n);
+}
