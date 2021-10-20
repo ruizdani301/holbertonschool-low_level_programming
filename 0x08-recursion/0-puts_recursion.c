27b331483a61c9bd6af17d22e472cdbf27c01a21
ruizdani301 <ruizdani301@gmail.com>
2021-10-20 11:15:58 -0500

    recursion

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _puts_recursion - check the code
+ * @s:variable
+ * Return: Always 0.
+ */
+void _puts_recursion(char *s)
+
+{
+int i = 0;
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		_putchar(s[i]);
+	}
+	_putchar(10);
+}
