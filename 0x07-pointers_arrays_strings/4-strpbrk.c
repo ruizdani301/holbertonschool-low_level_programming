c83f6840f622bc65bddf04d885c2ad6775170c6b
ruizdani301 <ruizdani301@gmail.com>
2021-10-19 19:16:36 -0500

    str

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,41 @@
+#include "main.h"
+#include <stdio.h>
+/**
+* _strpbrk - toupper.
+* @s: variable
+* @accept: variable
+* Return: (data)
+*/
+char *_strpbrk(char *s, char *accept)
+{
+	int i, x = 0;
+	char ult;
+	int f = 0;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (x = 0; accept[x] != '\0'; x++)
+
+		{
+			if (s[i] == accept[x])
+			{
+				f = 1;
+				s = &s[i];
+				break;
+			}
+		}
+		if (f == 1)
+		{
+			break;
+		}
+	}
+	ult = s[i + 1];
+	if (ult == '\0')
+	{
+		return ('\0');
+	}
+	else
+	{
+		return (s);
+	}
+}
