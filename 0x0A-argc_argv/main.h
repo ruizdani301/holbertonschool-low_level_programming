d3c58ffe2d7ee9536936c609706cbf60c680f72a
ruizdani301 <ruizdani301@gmail.com>
2021-10-22 15:05:19 -0500

    header

---
+++ 0x0A-argc_argv/main.h
@@ -0,0 +1,11 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc __attribute__((unused)), char *argv[]);
+int main(int argc __attribute__((unused)), char * argv[]);
+int main(int argc, char * argv[]);
+
+#endif /* MAIN_H */
