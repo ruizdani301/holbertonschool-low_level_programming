fb004418a2173e0d36d89a2c4a377ab23a800f05
ruizdani301 <ruizdani301@gmail.com>
2021-10-22 14:58:32 -0500

    myname

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - parameters main
+ * @argc:variable
+ * @argv: variable
+ * Return: Always Data.
+ */
+int main(int argc __attribute__((unused)), char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
