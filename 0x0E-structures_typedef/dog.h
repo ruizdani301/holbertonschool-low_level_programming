04eca4386037fae13b077ad1ecbb9356a632c66e
ruizdani301 <ruizdani301@gmail.com>
2021-11-01 17:58:12 -0500

    dog

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,16 @@
+#ifndef dog_H
+#define dog_H
+/**
+ * struct dog - Short description
+ *@name: variable
+ *@owner:variable
+ *@age:variable
+ * Description: Longer description
+ */
+struct dog
+{
+	char *name;
+	char *owner;
+	float age;
+};
+#endif /* dog */
