900d2ab4eca8493085d802393adda77bfe79c398
ruizdani301 <ruizdani301@gmail.com>
2021-11-05 10:46:06 -0500

    string

---
+++ 0x10-variadic_functions/2-print_strings.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdarg.h>
+/**
+ * print_strings - print char and numbers
+ * @n: variable
+ * @separator: pointer
+ * Return: data
+ */
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	unsigned int i = 0;
+	char  *valor;
+	va_list list;
+
+	va_start(list, n);
+	for (i = 0; i < n; i++)
+	{
+		valor = va_arg(list, char *);
+
+		if (valor != NULL)
+		{
+			printf("%s", valor);
+		}
+		else
+		{
+			printf("nil");
+		}
+		if (separator != NULL && i < (n - 1))
+		{
+			printf("%s", separator);
+		}
+	}
+	printf("\n");
+	va_end(list);
+}
