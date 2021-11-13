75fa275b6ca0edc118122b85e742eb5cf13cde30
ruizdani301 <ruizdani301@gmail.com>
2021-11-12 21:18:41 -0500

    lists

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,21 +1,18 @@
+#ifndef LISTS_H
+#define LISTS_H
+
 #include <stdlib.h>
 #include <string.h>
 #include <stdio.h>
 
-#ifndef LISTS_H
-#define LISTS_H
-
-#include <stdio.h>
-#include <stdlib.h>
-
-  /**
-    * struct list_s - singly linked list
-    * @str: string - (malloc'ed string)
-    * @len: length of the string
-    * @next: points to the next node
-    *
-    * Description: singly linked list node structure
-    */
+/**
+ * struct list_s - singly linked list
+ * @str: string - (malloc'ed string)
+ * @len: length of the string
+ * @next: points to the next node
+ *
+ * Description: singly linked list node structure
+ */
 
 typedef struct list_s
 {
