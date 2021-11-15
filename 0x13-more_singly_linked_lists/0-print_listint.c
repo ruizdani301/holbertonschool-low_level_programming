7eb293ff14d58d27b9f98e7a6ad3b88886d2cb97
ruizdani301 <ruizdani301@gmail.com>
2021-11-15 12:06:56 -0500

    print

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+/**
+ * print_listint
+ * @h: pointer
+ * Return: Data
+ */
+
+size_t print_listint(const listint_t *h)
+{
+	size_t x = 0;
+
+	while (h != NULL)
+	{
+		printf("%d\n", h->n);
+		h = h->next;
+		x++;
+	}
+	return (x);
+}
