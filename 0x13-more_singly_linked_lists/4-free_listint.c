0221f4e048b33204e3b0a78dd3bdd218f11b0a87
ruizdani301 <ruizdani301@gmail.com>
2021-11-16 10:06:17 -0500

    free

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+
+/**
+ * free_listint - free list
+ * @head: pointer
+*/
+void free_listint(listint_t *head)
+{
+	listint_t *tmp;
+
+	while (head != NULL)
+	{
+		tmp = head;
+		head = head->next;
+		free(tmp);
+	}
+}
