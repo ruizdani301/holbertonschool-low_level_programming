158bc6a902eb9b4fdfa40a6fd96892502bbd4f37
ruizdani301 <ruizdani301@gmail.com>
2021-11-16 11:11:11 -0500

    free

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+ * free_listint2 -  free the list and set the header
+ * @head: pointer
+ */
+
+void free_listint2(listint_t **head)
+{
+	listint_t *tmp;
+
+	if (*head == NULL)
+		return;
+
+	while ((*head) != NULL)
+	{
+		tmp = (*head);
+		*head = (*head)->next;
+		free(tmp);
+	}
+	*head = NULL;
+}
