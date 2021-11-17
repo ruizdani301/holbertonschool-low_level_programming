57ce73a9571bfa83c35b864bf9a2cc7d27f43706
ruizdani301 <ruizdani301@gmail.com>
2021-11-17 16:41:33 -0500

    insert

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -22,7 +22,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	if (idx != 0)
 	{
 		tmp = (*head);
-		while (x != idx)
+		while (x < idx && tmp != NULL)
 		{
 			tmp = tmp->next;
 			x++;
