b4dfbd026b15b023ec8dd395b4e214b47df3ef16
ruizdani301 <ruizdani301@gmail.com>
2022-01-06 14:22:03 -0500

    0x17

---
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,16 @@
+#include "lists.h"
+/**
+ * dlistint_len -  returns the number of elements
+ * @h: pointer
+ * Return:(i)
+ */
+size_t dlistint_len(const dlistint_t *h)
+{
+        size_t i = 0;
+        while(h != NULL)
+        {
+            h = h->next;
+            i++;
+        }
+        return (i);
+}
