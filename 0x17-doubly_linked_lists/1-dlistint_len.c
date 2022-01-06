081a432fca32477af79af9fa01730e1f1ff0f536
ruizdani301 <ruizdani301@gmail.com>
2022-01-06 14:24:36 -0500

    1

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -5,12 +5,12 @@
  * Return:(i)
  */
 size_t dlistint_len(const dlistint_t *h)
-{
-        size_t i = 0;
-        while(h != NULL)
-        {
-            h = h->next;
-            i++;
-        }
-        return (i);
+    {
+    size_t i = 0;
+    while(h != NULL)
+    {
+    h = h->next;
+    i++;
+    }
+    return (i);
 }
