c4032be1e111861f4fcb80dfb7db18f11b3cfced
ruizdani301 <ruizdani301@gmail.com>
2022-01-06 20:24:03 -0500

    5

--- 0x17-doubly_linked_lists/5-get_dnodeint.c
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -8,21 +8,18 @@
 dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 {
 	unsigned int i = 0;
-	dlistint_t *tmp;
 
 	if (head == NULL)
 	{
 		return (NULL);
 	}
-	tmp = head;
-	while (tmp != NULL)
+	while (head != NULL && i < index)
 	{
-		tmp = tmp->next;
+		head = head->next;
 		i++;
-		if (i == index)
-		{
-			return (tmp);
-		}
 	}
-	return (NULL);
+	if (head == NULL)
+		return (NULL);
+	else
+		return (head);
 }
