19f3cdedeaad2a8bdd5bda1cf6905b588d818536
ruizdani301 <ruizdani301@gmail.com>
2022-01-10 13:50:41 -0500

    18

--- 0x18-dynamic_libraries/100-atoi.c
+++ 0x18-dynamic_libraries/100-atoi.c
@@ -1,4 +1,9 @@
 #include "main.h"
+/**
+* _atoi - function cast variables
+* @s: variable
+* Return: 0
+*/
 int _atoi(char *s)
 {
 	(void)s;
--- 0x18-dynamic_libraries/5-strstr.c
+++ 0x18-dynamic_libraries/5-strstr.c
@@ -1,3 +1,11 @@
+#include "main.h"
+#include <stdio.h>
+/**
+* _strstr - toupper.
+* @haystack: variable
+* @needle: variable
+* Return: (data)
+*/
 char *_strstr(char *haystack, char *needle)
 {
 	(void)haystack;
