c27ce39b1f02a64d33d3c926a49615f4e0d15374
ruizdani301 <ruizdani301@gmail.com>
2022-01-29 10:32:59 -0500

    m

--- 0x1A-hash_tables/2-key_index.c
+++ 0x1A-hash_tables/2-key_index.c
@@ -8,7 +8,10 @@
 */
 unsigned long int key_index(const unsigned char *key, unsigned long int size)
 {
-	unsigned int indice = 0;
+	unsigned long int indice = 0;
+
+	if (size == 0)
+		return (0);
 	indice = hash_djb2(key) % size;
 	return (indice);
 }
