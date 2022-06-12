a0dd21c0daee33d0f08eb9d68dfcf9bd993f4121
ruizdani301 <ruizdani301@gmail.com>
2022-06-12 11:17:03 -0500

    all task

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -2,37 +2,37 @@
 #include <stdlib.h>
 #include "search_algos.h"
 /**
-* linear_search- method name
-* @array: array
-* @size: size
-* @value: value
-* Return: return value
+ * binary_search- method name
+ * @array: array
+ * @size: size
+ * @value: value
+ * Return: return value
 */
 int binary_search(int *array, size_t size, int value)
 {
 	int num = 0;
-    int inicio = 0;
-    int final = (int)size − 1;
-    int mitad = 0;
+	int inicio = 0;
+	int final = (int)size - 1;
+	int mitad = 0;
 
 	if (array == NULL)
 		return (-1);
-    
-    while (inicio <= final)
-    {
-        mitad = ((inicio + final) / 2);
-        printf("Searching in array");
-        for (num = inicio; num < final; num++)
-        {
-            printf("%d, ", array[num]);
-        }
-        printf("%d\n", array[num]);
-        if (array[mitad] < value)
-            inicio = mitad + 1
-        else if (array[mitad] > value)
-            final = mitad − 1
-        else:
-            return mitad
-    }
+
+	while (inicio <= final)
+	{
+		mitad = ((inicio + final) / 2);
+		printf("Searching in array");
+		for (num = inicio; num < final; num++)
+		{
+			printf("%d, ", array[num]);
+		}
+		printf("%d\n", array[num]);
+		if (array[mitad] < value)
+			inicio = mitad + 1;
+		else if (array[mitad] > value)
+			final = mitad - 1;
+		else
+			return (mitad);
+	}
 	return (-1)
 }
